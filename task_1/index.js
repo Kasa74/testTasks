4a85ff13b6afda260aee43465966d5c45c11d9bc
Nikita <niki.kudryavcev@gmail.com>
2024-04-21 18:38:45 +0300

    task-1 solution

--- task_1/index.js
+++ task_1/index.js
@@ -1,8 +1,29 @@
-import {encoded, translations} from './data.js'
+import { encoded, translations } from "./data.js";
 
-console.log("Let's rock")
-console.log(encoded, translations)
+import { encoded, translations } from "./data.js";
 
+const EXCEPTIONS = ["groupId", "service", "formatSize", "ca"];
+const uniqIds = [];
 
+encoded.forEach((obj) => {
+  Object.keys(obj).forEach((key) => {
+    const id = Number(obj[key]);
+
+    if (isNaN(id)) {
+      return;
+    }
+    const hasId = translations.hasOwnProperty(id);
+
+    if (hasId && !EXCEPTIONS.includes(obj[key])) {
+      obj[key] = translations[id];
+    }
+    if (!hasId && !uniqIds.includes(id)) {
+      uniqIds.push(id);
+    }
+  });
+});
+
+console.log(encoded);
+console.log(uniqIds);
 
 // console.log(decoded)
